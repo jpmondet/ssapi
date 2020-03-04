8bb2d516f58f5056406269445ecebe3c24f75157
jpmondet <jp@mondet.org>
2020-03-04 09:52:42 +0100

    Add proper building script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='topN',
+      version='0.01',
+      description='scrapping ScoreSaber API to calculate top countries',
+      author='jpmondet',
+      author_email='github.com/jpmondet',
+      url='github.com/jpmondet/ssapi',
+      packages=['topN'],
+     )
