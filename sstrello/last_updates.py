edbec4ace8e36d391399db811c85196cd0340382
jpmondet <jp@mondet.org>
2020-12-27 07:51:49 +0100

    [trello updates] Use 'rich' to get a nice output and mypy'd the code

--- sstrello/last_updates.py
+++ sstrello/last_updates.py
@@ -1,18 +1,39 @@
 #! /usr/bin/env python3
 
 from requests import get
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+from typing import List, Dict, Any
 
-SSID = "5f0eddb5738be96a56b660e7" 
-BOARD_URL = f"https://api.trello.com/1/boards/{SSID}"
-CARDS_INFOS = f"{BOARD_URL}/cards"
+SSID: str = "5f0eddb5738be96a56b660e7" 
+BOARD_URL: str = f"https://api.trello.com/1/boards/{SSID}"
+CARDS_INFOS: str = f"{BOARD_URL}/cards"
 
 
-def main():
-    cards_infos = get(CARDS_INFOS).json()
-    sorted_cards = sorted(cards_infos, key=lambda x: x["dateLastActivity"])
-    for card in sorted_cards:
-        print()
-        print(card["name"], card["dateLastActivity"])
+def main() -> None:
+    console: Console = Console()
+
+    cards_infos: List[Dict[str, Any]] = get(CARDS_INFOS).json()
+
+    sorted_cards: List[Dict[str, Any]] = sorted(cards_infos, key=lambda x: x["dateLastActivity"], reverse=True)
+
+    table: Table = Table(show_header=True, header_style="bold blue")
+    table.add_column("Last Updates", style="dim", width=25)
+    table.add_column("Name Card")
+
+    for index, card in enumerate(sorted_cards[:10]):
+        #console.print(f"[bold cyan]{card['name']}[/bold cyan] [bold red]{card['dateLastActivity']}[/bold red]")
+        last: str = card['dateLastActivity']
+        name: Text = Text(card['name'])
+        if index == 0:
+            name.stylize("bold green")
+            last = f"[bold cyan]{card['dateLastActivity']}[/bold cyan]"
+        table.add_row(last, name)
+
+    console.print()
+    console.print("[bold white underline]ScoreSaber's Trello updates")
+    console.print(table)
 
 
 if __name__ == "__main__":
